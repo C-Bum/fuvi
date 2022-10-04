8f95d0bf15ef4f4300d062c3e8f96032d5740cd5
Joel perez <joeljpa2004@gmail.com>
2022-10-04 10:48:48 -0600

    se modifico el archivo constantes_joelperez.cpp y se agrego pitagora.cpp

---
+++ pitagora.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+	
+	double cateto1, cateto2, hipotenusa; 
+	cout<<"ingrese la cateto 1: "; cin>> cateto1;
+	cout<<"ingrese la cateto 2: "; cin>> cateto2;
+	
+	hipotenusa = sqrt(pow(cateto1,2)) + pow(cateto2,2);
+	
+	cout<<"cateto 1 = " << cateto1 <<endl;
+	cout<<"cateto 2 = " << cateto2 <<endl;
+	
+	cout<<"hipotenusa = " << hipotenusa <<endl;
+	return 0;
+	
+}
