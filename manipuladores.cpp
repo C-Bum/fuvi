843740817e0ba62ccb905e750325523326312fdf
Joel perez <joeljpa2004@gmail.com>
2022-10-06 08:29:14 -0600

    Se agrego  el archivo manipuladores.cpp

---
+++ manipuladores.cpp
@@ -0,0 +1,19 @@
+//Manipuladores
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main (){
+	
+	cout <<left;
+	cout << setw(20) << "Artista" << setw(10) << "Album" <<endl
+         << setw(20) << "Mac de Marco" <<setw(10)  << "For the firs time" <<endl;
+         
+         cout<< fixed << setprecision(2) << 12.34467;
+     
+         
+	    
+	return 0;
+	
+}
